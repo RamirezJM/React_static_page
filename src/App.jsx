3bf7a447e7b8974c9b3762076e84382174d93503
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-04-08 23:28:03 -0400

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 
-import reactLogo from './assets/react.svg'
-
+import Header from './components/Header'
+import Main from './components/Main'
 import './App.css'
 
 function App() {
   return (
       <div className='container'>
-        <header>
-        <img src={reactLogo} alt="react logo" />
-        <p>ReactFacts</p>
-        </header>
-        <main>
-          <h1>Fun facts about React</h1>
-          <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 200K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-          </ul>
-        </main>
-        
-
-        
+        <Header/>
+        <Main/>       
       </div>
-      
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import reactLogo from '../assets/react.svg'
+
+export default function Header(){
+  return(
+    <header>
+    <img src={reactLogo} alt="react logo" />
+    <p>ReactFacts</p>
+    </header>
+  )
+}
